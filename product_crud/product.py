52229bdd08997c9f32b66ce0a3183d183e256f0f
Babak Jahangiri <babakjahangiri@gmail.com>
2022-12-26 12:09:50 +0000

    write body of the class with body crud methods

---
+++ product_crud/product.py
@@ -0,0 +1,40 @@
+class Product():
+    def __init__(self, title:str, short_description:str , description:str  , slug:str, permalink:str, sku:str, price:float, regular_price:float,
+                 sale_price:float, manage_stock:bool, stock_quantity:int, date_created_gmt :int, date_modified_gmt:int,category_id:int = 0, 
+                 is_visible = True, is_available:bool = False):
+
+        self.id = ''
+        self.category_id = category_id
+        self.title = title
+        self.short_description =  short_description
+        self.description = description
+        self.slug = slug
+        self.permalink = permalink
+        self.available = is_available
+        self.sku = sku
+        self.price = price
+        self.regular_price = regular_price
+        self.sale_price = sale_price
+        self.manage_stock = manage_stock
+        self.stock_quantity = stock_quantity
+        self.is_visible = is_visible
+        self.date_created_gmt = date_created_gmt
+        self.date_modified_gmt = date_modified_gmt
+       
+
+    def create(self):
+        pass
+
+
+    def read(self):
+        pass
+
+
+    def update(self):
+        pass
+
+
+    def delete(self):
+        pass
+
+
