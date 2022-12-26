e9b7846d7a21a91af38da1d6487f8e00ffb6cba4
Babak Jahangiri <babakjahangiri@gmail.com>
2022-12-26 13:04:46 +0000

    write dunder __repr__ method for product to show it in correct format

--- product_crud/main.py
+++ product_crud/main.py
@@ -1,5 +1,28 @@
+import datetime
+from product import Product
+
 def main():
     print('Hello World')
+    
+    currentdatetime = datetime.datetime.utcnow()
+    current_unixtimestamp = int(currentdatetime.timestamp())
+
+    theProduct = Product('Lenovo T410s',
+        'Lenovo ThinkPad T410s Core i5 M560 2.66GHz 4GB RAM, WIN 10 14" AC ADAPTER',
+        'The T410s provides a good companion for office use, that is well suited for business trips thanks to its 14.1 inch size and light weight.',
+        'thinkbook-13x-gen-2-(13-inch-intel)',
+        'https://www.lenovo.com/gb/en/p/laptops/thinkbook/thinkbookx/thinkbook-13x-gen-2-(13-inch-intel)/21atcto1wwgb2?cid=gb:sem|se|google|G-UK-SEM-COMMERCIAL-PUBLIC-CCF-LF-Shopping-PLA-Brand-Commercial|Brand-CommercialLaptops-Intel',
+        '21ATCTO1WWGB2',
+        799.95,
+        849.95,
+        0,
+        False,
+        4,
+        current_unixtimestamp,
+        current_unixtimestamp,
+        1)
+ 
+    print(theProduct.__repr__())
 
 if __name__ == '__main__':
     # This code won't run if this file is imported.
--- product_crud/product.py
+++ product_crud/product.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class Product():
     def __init__(self, title:str, short_description:str , description:str  , slug:str, permalink:str, sku:str, price:float, regular_price:float,
                  sale_price:float, manage_stock:bool, stock_quantity:int, date_created_gmt :int, date_modified_gmt:int,category_id:int = 0, 
@@ -10,7 +12,7 @@ class Product():
         self.description = description
         self.slug = slug
         self.permalink = permalink
-        self.available = is_available
+        self.is_available = is_available
         self.sku = sku
         self.price = price
         self.regular_price = regular_price
@@ -38,3 +40,23 @@ class Product():
         pass
 
 
+    def __repr__(self):
+        return f"the product with \n\
+        Product Id: N/A \n\
+        Title: {self.title} \n\
+        Short description: {self.short_description} \n\
+        Description: {self.description} \n\
+        Slug: {self.slug} \n\
+        Permanent link: {self.permalink} \n\
+        availablity: {self.is_available} \n\
+        Stock keeping Unit: {self.sku} \n\
+        Price: {self.price} \n\
+        Reqular Price: {self.regular_price} \n\
+        Sale Price: {self.sale_price} \n\
+        Manage Stock {self.manage_stock} \n\
+        Stock Quantity: {self.stock_quantity} \n\
+        Visible: {self.is_visible} \n\
+        Date Created: {datetime.utcfromtimestamp(self.date_created_gmt).strftime('%Y-%m-%d %H:%M:%S')} \n\
+        Date Modified: {datetime.utcfromtimestamp(self.date_modified_gmt).strftime('%Y-%m-%d %H:%M:%S')} \n\
+        "
+       
